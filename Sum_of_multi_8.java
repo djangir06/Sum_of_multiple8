e4b2c9613fa7aca8889d10522aba441d80ca6602
Dheeraj Jangir <d.jangir06092003@gmail.com>
2025-07-09 22:12:46 +0530

    Add files via upload

---
+++ Sum_of_multi_8.java
@@ -0,0 +1,28 @@
+
+public class Sum_of_multi_8 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for(int i =1; i<=10;i++){
+
+                sum += 8*i;
+
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
